d53b303c9e04e0cb61a7e2953accb0de0ce46f0e
anpetry-admin <anpetry@htwsaar.de>
2025-05-28 23:25:20 +0200

    no message

--- run_and_sync_forecast_PG_2024_1.py
+++ run_and_sync_forecast_PG_2024_1.py
@@ -123,7 +123,7 @@ def subset_and_upload(grib_path):
         print(f"[CLEANUP] Deleted GRIB file: {grib_path}")
 
 def run_forecasts():
-    start_date = datetime(2024, 2, 17)
+    start_date = datetime(2024, 3, 7)
     end_date = datetime(2024, 6, 30)
     lead_time = 168
     time_str = "1200"
